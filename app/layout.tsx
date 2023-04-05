61a9924ddd285efcc11b9143deadb604eecbba75
mxnf131 <128838337+mxnf131@users.noreply.github.com>
2023-04-05 17:35:42 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,10 @@ try {
 }
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "ChatGPT Pro版",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "ChatGPT Pro版",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
